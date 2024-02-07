404e969b3e1c7cb970629ddd04ccf3c6ccd5029a
Shreyas K <Shreyas K@TYSS-Shreyas-K>
2024-02-07 17:15:54 +0530

    First Commit

---
+++ GitHubPractise/src/main/java/genericLibrary/Shreyas.java
@@ -0,0 +1,21 @@
+package genericLibrary;
+
+public class Shreyas {
+
+	public void play() {
+		String name="shreyas";
+		String game="volleyball";
+		System.out.println(name+" "+game);
+	}
+	
+	public void eat() {
+		String food="masala dosa";
+		System.out.println(food);
+	}
+	
+	public void dance() {
+		String song="chamak challo";
+		System.out.println(song);
+	}
+	
+}
